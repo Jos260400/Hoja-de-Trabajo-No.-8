80b5df02b38b0f6cbc699869c0a5d3d5767857aa
José Ovando <42974480+Jos260400@users.noreply.github.com>
2019-03-29 19:34:39 -0600

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,34 @@
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+
+/**
+ * @author DARKTONE
+ *
+ */
+public class Test extends TestCase {
+
+	/**
+	 * @param name
+	 */
+	public Test(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
